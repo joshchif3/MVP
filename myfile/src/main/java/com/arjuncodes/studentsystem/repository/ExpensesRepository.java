252a536c579349393313fa3fb29f74b200a15ea5
joshchif3 <pantherchif3@gmail.com>
2024-09-21 23:07:56 +0200

    my files

--- myfile/src/main/java/com/arjuncodes/studentsystem/repository/ExpensesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.arjuncodes.studentsystem.repository;
-
-import com.arjuncodes.studentsystem.model.Expenses;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpensesRepository extends JpaRepository<Expenses, Integer> {
-}
