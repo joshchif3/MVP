a3ef377b449f912308e127becc22523c7f8e1801
joshchif3 <pantherchif3@gmail.com>
2024-09-22 13:56:47 +0200

    first commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Ensure this matches your GitHub Pages URL path
+  base: '/frontendtest/',  // Ensure this matches your GitHub Pages URL path
 });
